48ffc979197c1a95669754ca065a4f61999efbee
Luigi Laezza <luigi@soiposervices.com>
2023-08-09 10:57:23 -0300

    Added second migration

---
+++ database/migrations/2023_08_09_122306_update_cms_posts_table_change_schedule_for.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCmsPostsTableChangeScheduleFor extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('posts', function (Blueprint $table) {
+            $table->timestamp('scheduled_at')->useCurrent()->nullable()->after('published');
+            $table->dropColumn('schedule_for');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->timestamp('scheduled_for')->useCurrent()->after('published');
+            $table->dropColumn('schedule_at');
+        });
+    }
+}
